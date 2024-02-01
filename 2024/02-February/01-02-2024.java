cd39e0e5ad5b575823d9959da205f5a3db8c9f3b
chinmoy1809 <chinmoycode74@gmail.com>
2024-02-01 20:53:12 +0530

    01-02-2024

--- 2024/02-February/01-02-2024.java
+++ 2024/02-February/01-02-2024.java
@@ -1,1 +1,32 @@
 
+
+// Name : Chinmoy Das
+// Date : 01/02/2024
+// Problem :  Divide Array Into Arrays With Max Difference
+// Difficulty level : Medium
+// Problem Link : https://leetcode.com/problems/divide-array-into-arrays-with-max-difference/
+
+
+import java.util.Arrays;
+
+class Solution {
+    public int[][] divideArray(int[] nums, int k) {
+        int result[][] = new int[nums.length / 3][3];
+        if (nums.length % 3 != 0)
+            return new int[][] {};
+        Arrays.sort(nums);
+
+        int x = 0;
+        for (int i = 0; i < nums.length; i += 3) {
+            if ((i + 2 < nums.length) && (nums[i + 2] - nums[i]) <= k) {
+                result[x][0] = nums[i];
+                result[x][1] = nums[i + 1];
+                result[x][2] = nums[i + 2];
+                x++;
+            } else {
+                return new int[][] {};
+            }
+        }
+        return result;
+    }
+}
