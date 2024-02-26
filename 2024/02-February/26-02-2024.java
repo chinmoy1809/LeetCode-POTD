67b60461cadb73342e8f94e4e168f2c7259ea072
chinmoy1809 <chinmoycode74@gmail.com>
2024-02-26 16:20:29 +0530

    26-02-2024

---
+++ 2024/02-February/26-02-2024.java
@@ -0,0 +1,35 @@
+
+// Name : Chinmoy Das
+// Date : 26/02/2024
+// Problem : Same-tree
+// Difficulty level : Easy
+// Problem Link : https://leetcode.com/problems/same-tree/
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {
+    }
+    TreeNode(int val) {
+        this.val = val;
+    }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+        if (p == null || q == null)
+            return false;
+        return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
+
+// Thankyou❤️‍🩹
