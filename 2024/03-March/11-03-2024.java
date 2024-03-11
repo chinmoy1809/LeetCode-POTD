58c7a74aa768c1608345e7beb8cc1613b998cf8f
chinmoy1809 <chinmoycode74@gmail.com>
2024-03-11 14:19:09 +0530

    11-03-2024

---
+++ 2024/03-March/11-03-2024.java
@@ -0,0 +1,30 @@
+
+// Name : Chinmoy Das
+// Date : 11/03/2024
+// Problem : Custom-sort-string
+// Difficulty level : Medium
+// Problem Link : https://leetcode.com/problems/custom-sort-string/
+
+import java.util.HashMap;
+
+class Solution {
+    public String customSortString(String order, String s) {
+        StringBuilder result = new StringBuilder();
+        HashMap<Character, Integer> mp = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            mp.put(c, mp.getOrDefault(c, 0) + 1);
+        }
+        for (char c : order.toCharArray()) {
+            if (mp.containsKey(c)) {
+                result.append(String.valueOf(c).repeat(Math.max(0, mp.get(c))));
+                mp.remove(c);
+            }
+        }
+        for (char c : mp.keySet()) {
+            result.append(String.valueOf(c).repeat(Math.max(0, mp.get(c))));
+        }
+        return result.toString();
+    }
+}
+
+// Thankyou❤️‍🩹
