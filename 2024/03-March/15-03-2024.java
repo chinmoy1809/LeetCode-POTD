6d9d70cf3c9b8ff157589ce19826b77c01b5616b
chinmoy1809 <chinmoycode74@gmail.com>
2024-03-15 21:15:52 +0530

    15-03-2024

---
+++ 2024/03-March/15-03-2024.java
@@ -0,0 +1,26 @@
+
+// Name : Chinmoy Das
+// Date : 15/03/2024
+// Problem : Product-of-array-except-self
+// Difficulty level : Medium
+// Problem Link :https://leetcode.com/problems/product-of-array-except-self/
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int result[] = new int[n];
+        result[0] = 1;
+        for (int i = 1; i < n; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+        int suffixProduct = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] *= suffixProduct;
+            suffixProduct *= nums[i];
+        }
+
+        return result;
+    }
+}
+
+// Thankyou❤️‍🩹
