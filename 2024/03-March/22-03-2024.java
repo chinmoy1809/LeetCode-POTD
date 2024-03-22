f28de2c80a0929484b6ba0e5442554f466dc0b94
chinmoy1809 <chinmoycode74@gmail.com>
2024-03-22 13:17:09 +0530

    22-03-2024

---
+++ 2024/03-March/22-03-2024.java
@@ -0,0 +1,38 @@
+
+// Name : Chinmoy Das
+// Date : 22/03/2024
+// Problem : Palindrome-linked-list
+// Difficulty level : Easy
+// Problem Link : https://leetcode.com/problems/palindrome-linked-list/
+
+import java.util.*;
+
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int d) {
+        data = d;
+        left = right = null;
+    }
+}
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> list = new ArrayList();
+        while(head != null) {
+            list.add(head.val);
+            head = head.next;
+        }
+        
+        int left = 0;
+        int right = list.size()-1;
+        while(left < right && list.get(left) == list.get(right)) {
+            left++;
+            right--;
+        }
+        return left >= right;
+    }
+}
+
+// Thankyou❤️‍🩹
